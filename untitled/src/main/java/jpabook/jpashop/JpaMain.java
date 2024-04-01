70fef5e9e2125bd2cf2c293b58700e5762b0f896
Seohyeon.Lim <forestlight749@gmail.com>
2024-04-02 00:23:07 +0900

    INIT

---
+++ untitled/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction(); // tx 얻기
+        tx.begin();
+
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback(); // 문제가 생기면 철회
+        } finally {
+            em.close(); // 작업이 끝나면 EntityManager를 닫아줌.
+        }
+        // 전체 애플리케이션이 완전히 끝나면 EntityManagerFactory를 닫아주어야 함.
+        emf.close();
+    }
+}
